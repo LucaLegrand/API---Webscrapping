112348f16561bb9ea24757781276b77097d19b5e
Luca2503 <98313558+Luca2503@users.noreply.github.com>
2024-12-03 15:07:10 +0100

    Step 12: Prediction with Trained Model

--- TP2 and  3/services/epf-flower-data-science/src/api/router.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/router.py
@@ -1,10 +1,10 @@
 """API Router for Fast API."""
 from fastapi import APIRouter
 
-from src.api.routes import hello, data
+from src.api.routes import hello, data, model
 
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
 router.include_router(data.router, tags=["Data"])
-router.include_router(data.router, tags=["Model"])
+router.include_router(model.router, tags=["Model"])
